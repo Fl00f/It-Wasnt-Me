3e285ec1046cd02762f4d7371948c56c0b54743c
James Robinson <deltaohio3545@yahoo.com>
2016-12-11 00:58:09 -0500

    minor

---
+++ Assets/TimeCaptureTest.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TimeCaptureTest : MonoBehaviour
+{
+
+	public static List<Vector3> RecordedMovData;
+	public static List<Quaternion> RecordedRotData;
+	int count = 0;
+	float framesPerSecound = 30f;
+	float timer = 0;
+
+
+	public static bool Recording = false;
+	public static bool PlayingBack = false;
+
+	bool isMinion = true;
+	// Use this for initialization
+	void Start ()
+	{
+		if (GetComponent<Player>()) {
+			RecordedMovData = new List<Vector3> ();
+			RecordedRotData = new List<Quaternion> ();
+			isMinion = false;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (isMinion && !playBackData ()) {
+			return;
+		}
+
+		if (!isMinion && !recordData ()) {
+			return;
+		}
+
+
+		if (Input.GetKeyDown(KeyCode.I)) {
+			Recording = true;
+			PlayingBack = false;
+		}
+
+		if (Input.GetKeyDown(KeyCode.O)) {
+			Recording = false;
+			PlayingBack = true;
+		}
+
+		count = 0;
+
+	}
+
+
+	bool recordData ()
+	{
+		if (Recording) {
+			timer += Time.deltaTime;
+
+			if (timer > 1f / framesPerSecound) {
+				RecordedMovData.Add (transform.position);
+				RecordedRotData.Add (transform.rotation);
+				count++;
+				timer = 0;
+			}
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	bool playBackData ()
+	{
+
+		if (PlayingBack) {
+			timer += Time.deltaTime;
+			if (count != 0 && count < RecordedMovData.Count) {
+				transform.position = Vector3.Lerp (RecordedMovData [count - 1], RecordedMovData [count], (1f / framesPerSecound) * timer) * .2f;
+				transform.rotation = Quaternion.Lerp (RecordedRotData [count - 1], RecordedRotData [count], (1f / framesPerSecound) * timer);
+			}
+
+			if (timer > 1f / framesPerSecound) {
+//				transform.position = testing [count];
+				count++;
+				timer = 0;
+			}
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	int timePerRound = 60;
+	public static int CurrentTimeLeftInTimer = 0;
+	Text timerText;
+
+	string timerString = "Time: ";
+	// Use this for initialization
+	void Start () {
+		CurrentTimeLeftInTimer = timePerRound;
+		timerText = GetComponent<Text> ();
+		InvokeRepeating ("DeductTimeFromTimers", 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timerText.text = timerString + CurrentTimeLeftInTimer.ToString ();
+	}
+
+	void DeductTimeFromTimers(){
+		if (CurrentTimeLeftInTimer - 1 < 0) {
+			CurrentTimeLeftInTimer = 0;
+		} else {
+			CurrentTimeLeftInTimer -= 1;
+		}
+	}
+}
